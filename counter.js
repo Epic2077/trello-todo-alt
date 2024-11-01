891f4247b633bd76e25630cba475d0078dfdfa38
Ashkan <the.epic.2077@gmail.com>
2024-11-01 15:29:01 +0330

    Desplay func

--- counter.js
+++ counter.js
@@ -1,9 +1,35 @@
-export function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
-}
+// function cacheFunction(fn) {
+//   const cache = {};
+
+//   return function (input) {
+//     if (cache[input] !== undefined) {
+//       console.log("Returning cached result");
+//       return cache[input];
+//     }
+
+//     console.log("Calculating new result");
+//     const result = fn(input);
+//     cache[input] = result;
+//     return result;
+//   };
+// }
+
+// const slowCalculation = (num) => {
+//   for (let i = 0; i < 1e8; i++) {}
+//   return num * 2;
+// };
+// function slowCalculation2(num) {
+//   for (let i = 0; i < 2000; i++) {}
+//   return num + 10;
+// }
+
+// const cachedCalculation = cacheFunction(slowCalculation);
+
+// const cashedCalculation2 = cacheFunction(slowCalculation2);
+
+// console.log(cachedCalculation2(8));
+// console.log(cachedCalculation(5));
+// console.log(cachedCalculation(5));
+
+console.log(a2);
+const a2 = 2;
--- main.js
+++ main.js
@@ -1,24 +1,85 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+function toDoOption() {
+  const toDoOpt = document.getElementById("todo-options");
+  const optionsMenu = document.getElementById("options-menu");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+  toDoOpt.addEventListener("click", function () {
+    optionsMenu.classList.toggle("hidden");
+  });
+}
 
-setupCounter(document.querySelector('#counter'))
+toDoOption();
+
+function newTask() {
+  const newTask = document.getElementById("new-task");
+  let newCard = document.getElementById("new-card");
+  const optionsMenu = document.getElementById("options-menu");
+
+  newTask.addEventListener("click", function () {
+    if (!newCard.classList.contains("hidden")) {
+      alert("You have a new task menu open already");
+    } else {
+      newCard.classList.remove("hidden");
+      optionsMenu.classList.add("hidden");
+    }
+  });
+}
+
+newTask();
+
+function newTaskComplition() {
+  let store = [];
+  let time = new Date();
+  let close = document.getElementById("close");
+  let newCard = document.getElementById("new-card");
+  let title = document.getElementById("card-title-new");
+  let description = document.getElementById("card-des-new");
+  let submit = document.getElementById("submit");
+  let color = "white";
+  let titleValue = title.value;
+  let descValue = description.value;
+
+  close.addEventListener("click", function (cl) {
+    newCard.classList.add("hidden");
+    title.value = "";
+    descValue = "";
+  });
+  const colorDivs = document.querySelectorAll(".choose-color .colors");
+
+  colorDivs.forEach((div) => {
+    div.addEventListener("click", function () {
+      color = this.id;
+      newCard.classList.remove(
+        "bg-white",
+        "bg-red",
+        "bg-cyan",
+        "bg-green",
+        "bg-yellow"
+      );
+
+      newCard.classList.remove(`bg-white`);
+      newCard.classList.add(`bg-${color}`);
+    });
+  });
+
+  submit.addEventListener("click", function (sub) {
+    let colorValue = color;
+
+    let task = {
+      title: titleValue,
+      description: descValue,
+      color: colorValue,
+      timeCreated: time.toLocaleString(),
+    };
+    localStorage.setItem(titleValue, JSON.stringify(task));
+    newCard.classList.add("hidden");
+    title.value = "";
+    descValue = "";
+
+    displayTask(task);
+  });
+}
+newTaskComplition();
+
+function getData() {
+  console.log(localStorage.length);
+}
